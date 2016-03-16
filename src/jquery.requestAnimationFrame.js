044f909f6044c84b2dabe7c7d6766fe736a8be6f
Michał Gołębiowski <m.goleb@gmail.com>
2016-03-16 13:10:18 +0100

    Remove a comment about the polyfill that's no longer included

--- src/jquery.requestAnimationFrame.js
+++ src/jquery.requestAnimationFrame.js
@@ -17,12 +17,6 @@
 	}
 }(function (jQuery) {
 
-// requestAnimationFrame polyfill adapted from Erik Möller
-// fixes from Paul Irish and Tino Zijdel
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-
-
 var animating;
 
 function raf() {
